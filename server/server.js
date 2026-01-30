3b1f68ebb744f779dd5810a79e9195303812868a
Mohd Rameez Ansari <rameez@Mohds-MacBook-Air.local>
2026-01-31 01:17:37 +0530

    Add dotenv dependency

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
-import 'dotenv/config';
+import dotenv from "dotenv";
+import path from "path";
+
 import express from 'express';
-import { connect } from 'mongoose';
+import mongoose from "mongoose";
+
 import cors from 'cors';
 import { join, dirname } from 'path';
 import { existsSync, mkdirSync } from 'fs';
@@ -10,7 +13,11 @@ import { fileURLToPath } from 'url';
 import userRoutes from './routes/userRoutes.js';
 import reportRoutes from './routes/reportRoutes.js'; // <-- Import the new router
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+// const __dirname = dirname(fileURLToPath(import.meta.url));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.join(__dirname, ".env") });
 
 const app = express();
 
@@ -33,9 +40,6 @@ app.use('/api/reports', reportRoutes); // <-- Use the new report router
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
-connect(MONGO_URI)
-  .then(() => {
-    console.log('✅ MongoDB connected to', MONGO_URI);
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("MongoDB connection error:", err));
